27a4bcce8f973b454b2d4296f8cb54589fa33e9f
Amanda Tan <ahkaitan.68@gmail.com>
2019-02-27 01:54:16 +0000

    day 1

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
+  $.ajax({
+      url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response){
+          console.log(response);
+          console.log(response.data[0].images.original.url);
+          $("#display-gif").append(`<img src="${response.data[0].images.original.url}"/>`);
+          $(".text-center").hide();
+          
+          
+      }
+  })
+
   
 });
 
